fcd62b2c3c4038f62b3d4858eaf1fe61fb77870b
shpak <ket180@yandex.ru>
2025-08-08 10:17:30 +0300

    ADD average price, median, AND difference calculations

--- src/main/java/com/shpak/Main.java
+++ src/main/java/com/shpak/Main.java
@@ -7,10 +7,7 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static final String FILE_PATH = "src/main/resources/tickets.json";
@@ -21,10 +18,32 @@ public class Main {
         Main m = new Main();
         m.readFile();
         m.printMap();
+        m.avgVsMedian();
+    }
+
+    private void avgVsMedian() {
+        double avg = prices.stream()
+                .mapToDouble(a -> a)
+                .average()
+                .orElse(0.0);
+        double median = median();
+        System.out.printf("Разница между средней ценой и медианой составляет %1$,.2f р", Math.abs(avg - median));
+    }
+
+    private double median() {
+        Collections.sort(prices);
+        int size = prices.size();
+        if (size % 2 == 0)
+            return (prices.get(size / 2 - 1) + prices.get(size / 2) )/ 2.0;
+        else return prices.get(size / 2 - 1);
     }
 
     private void printMap() {
+        System.out.println("=".repeat(25));
+        System.out.println("Минимальное время полета между\n" +
+                "городами Владивосток и Тель-Авив:");
         minFlightTime.forEach((key, value) -> System.out.println(key + " " + value));
+        System.out.println("=".repeat(25));
     }
 
     private void readFile() {
